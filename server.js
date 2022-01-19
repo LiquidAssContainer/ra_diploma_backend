a002e91bd349566eee45bbb148c82e24813880e1
LiquidAssContainer <LiquidAssContainer@users.noreply.github.com>
2022-01-20 04:06:13 +0500

    Add server delay and error generation

--- server.js
+++ server.js
@@ -24,15 +24,15 @@ const randomNumber = (start, stop) => {
 
 const fortune = (ctx, body = null, status = 200) => {
   // Uncomment for delay
-  // const delay = randomNumber(1, 10) * 1000;
-  const delay = 0;
+  const delay = randomNumber(1, 10) * 1000;
+  // const delay = 0;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       // Uncomment for error generation
-      // if (Math.random() > 0.8) {
-      //     reject(new Error('Something bad happened'));
-      //     return;
-      // }
+      if (Math.random() > 0.8) {
+        reject(new Error("Something bad happened"));
+        return;
+      }
 
       ctx.response.status = status;
       ctx.response.body = body;
@@ -86,10 +86,11 @@ router.get("/api/items/:id", async (ctx, next) => {
 });
 
 router.post("/api/order", async (ctx, next) => {
+  console.log(JSON.parse(ctx.request.body));
   const {
     owner: { phone, address },
     items,
-  } = ctx.request.body;
+  } = JSON.parse(ctx.request.body);
   if (typeof phone !== "string") {
     return fortune(ctx, "Bad Request: Phone", 400);
   }
